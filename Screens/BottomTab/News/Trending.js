3e15ff375b5034cbe2e69b99bd6a916a6a0231f8
Shitanshu Tripathi <tripathis1106@gmail.com>
2023-10-04 19:39:32 +0530

    Update Trending.js

--- Screens/BottomTab/News/Trending.js
+++ Screens/BottomTab/News/Trending.js
@@ -1,6 +1,6 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
+import {AllStyles, fonts, Home, list} from '../../../Components/Styles';
 export default function Trending() {
   return (
     <View style={AllStyles.container}>
